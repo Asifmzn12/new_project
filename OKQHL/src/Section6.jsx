e114325edbcd0777026c21fc26f22dbcd2dfefd3
Asif Ali <asifmzn13@gmail.com>
2023-10-20 01:21:47 +0530

    some changes

--- OKQHL/src/Section6.jsx
+++ OKQHL/src/Section6.jsx
@@ -1,28 +1,160 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './TestimonialSlider.css'; // Import your CSS file for custom styles if needed
 
-function Section6() {
+const testimonials = [
+  {
+    id: 1,
+    name: 'John Doe',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the first testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 3,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  }, {
+    id: 4,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 1,
+    name: 'John Doe',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the first testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 3,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  }, {
+    id: 4,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  }, {
+    id: 1,
+    name: 'John Doe',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the first testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  {
+    id: 3,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  }, {
+    id: 4,
+    name: 'Jane Smith',
+    text: (
+      <span className="testimonial-text">
+        <span className="quotation-left">“</span>This is the second testimonial text.<span className="quotation-right">”</span>
+      </span>
+    ),
+  },
+  // Add more testimonial objects as needed
+];
+const TestimonialSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4, // Display 4 cards in one slide
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1200, // Adjust the breakpoint as needed
+        settings: {
+          slidesToShow: 3, // Display 3 cards in one slide on screens less than 1200px wide
+        },
+      },
+      {
+        breakpoint: 992, // Adjust the breakpoint as needed
+        settings: {
+          slidesToShow: 2, // Display 2 cards in one slide on screens less than 992px wide
+        },
+      },
+      {
+        breakpoint: 768, // Adjust the breakpoint as needed
+        settings: {
+          slidesToShow: 1, // Display 1 card in one slide on screens less than 768px wide
+        },
+      },
+    ],
+  };
+
+
   return (
-    <div className="bg-img-carusal">
-      <div className="container py-5">
-      <div className="title text-end">
-       <h5>Testimonials</h5>
-       <h4>Thoughts about our work</h4>
-       </div>
-       <div className="row py-5 justify-content-between align-content-center">
-
-       <div data-slick='{"slidesToShow": 2, "slidesToScroll": 4}'>
-         <div className="col-md-3">1</div>
-         <div className="col-md-3">2</div>
-         <div className="col-md-3">3</div>
-         <div className="col-md-3">4</div>
-
-        
-        </div>
-     </div>       
-       </div>
-      
-      
-      </div>
-  )
-}
-
-export default Section6
+    
+    <div className="testimonial-slider-container container py-5 ">
+      <Slider {...settings} className='py-5'>
+        {testimonials.map((testimonial) => (
+          <div key={testimonial.id} style={{ margin: '0 20px' }} className='box-shadow bg-white t'>
+            {/* Your card content here */}
+            <p>{testimonial.text}</p>
+            <h3>{testimonial.name}</h3>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+export default TestimonialSlider;
